b52e99a07ba3ff858cdbffcdeca94df01d2779ed
Omid Wiar <Omid.Wiar@infosupporttraining.onmicrosoft.com>
2022-06-12 14:28:02 +0200

    add request for new users and register method

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserRegisterRequest;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -17,13 +21,39 @@ class AuthController extends Controller
         return $request->all();
     }
 
-    // TODO: add description
-    public function register(Request $request)
+    /**
+     * Method register
+     *
+     * @param UserRegisterRequest $request
+     *
+     * @return JSON
+     */
+    public function register(UserRegisterRequest $request)
     {
-        // check if request is valid
+        // check if request is valid | 👍
         // check if user already exists
-        // store user in database
+        if (User::where('email', $request->email)->exists()) {
+            return response()->json([
+                'message' => 'Email already in use'
+            ], 200);
+        }
 
-        return $request->all();
+        // hash the password
+        $user = $request->toArray();
+        $user['password'] = Hash::make($request->password);
+
+        // store user in database
+        try {
+            User::create($user);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Whoops, something went wrong'
+            ], 500);
+        }
+
+        // return success message
+        return response()->json([
+            'message' => 'Successfully registered user'
+        ], 200);
     }
 }
